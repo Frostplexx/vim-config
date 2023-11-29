f45047b89c5f36988ee631ca8ec20bd78f88d64f
Daniel <daniel.inama02@gmail.com>
2023-11-29 12:50:23 +0100

    update keymap descriptions

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -13,21 +13,21 @@ map("n", "<S-Tab>", ":bprevious<CR>", { desc = "Go to previous buffer", remap = 
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Mark a file in Harpoon" })
+vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, { desc = "Toggle Haproon quick Menu" })
 
 vim.keymap.set("n", "<leader>1", function()
   ui.nav_file(1)
-end)
+end, { desc = "Go to 1. marked file" })
 vim.keymap.set("n", "<leader>2", function()
   ui.nav_file(2)
-end)
+end, { desc = "Go to 2. marked file" })
 vim.keymap.set("n", "<leader>3", function()
   ui.nav_file(3)
-end)
+end, { desc = "Go to 3. marked file" })
 vim.keymap.set("n", "<leader>4", function()
   ui.nav_file(4)
-end)
+end, { desc = "Go to 4. marked file" })
 
 -- Undotree
-vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+vim.keymap.set("n", "<leader>cu", vim.cmd.UndotreeToggle, { desc = "Toggle Undotree" })
