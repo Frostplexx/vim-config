3c2970d3e8ffa2ace9a8e83757ac21f629fffb99
Daniel <daniel.inama02@gmail.com>
2023-11-29 12:35:45 +0100

    Added some more plugins

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,3 +8,26 @@
 local map = vim.keymap.set
 map("n", "<Tab>", ":bnext<CR>", { desc = "Go to next buffer", remap = true, silent = true })
 map("n", "<S-Tab>", ":bprevious<CR>", { desc = "Go to previous buffer", remap = true, silent = true })
+
+-- harpoon
+local mark = require("harpoon.mark")
+local ui = require("harpoon.ui")
+
+vim.keymap.set("n", "<leader>a", mark.add_file)
+vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+
+vim.keymap.set("n", "<leader>1", function()
+  ui.nav_file(1)
+end)
+vim.keymap.set("n", "<leader>2", function()
+  ui.nav_file(2)
+end)
+vim.keymap.set("n", "<leader>3", function()
+  ui.nav_file(3)
+end)
+vim.keymap.set("n", "<leader>4", function()
+  ui.nav_file(4)
+end)
+
+-- Undotree
+vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
