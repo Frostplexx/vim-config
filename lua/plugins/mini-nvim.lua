64daa7cab4236aeb835e712e094b1eb8762a59f6
Daniel <daniel.inama02@gmail.com>
2024-02-17 13:40:24 +0100

    remove unneeded stuff

--- lua/plugins/mini-nvim.lua
+++ lua/plugins/mini-nvim.lua
@@ -136,59 +136,6 @@ return {
         -- [[ Mini Surround ]]
         require('mini.surround').setup()
 
-        -- -- [[ Mini Notify ]]
-        -- require('mini.notify').setup()
-        -- vim.notify = require('mini.notify').make_notify()
-
-        -- [[ Mini Cursorword ]]
-        require('mini.cursorword').setup()
-
-        local miniclue = require('mini.clue')
-        miniclue.setup({
-            triggers = {
-                -- Leader triggers
-                { mode = 'n', keys = '<Leader>' },
-                { mode = 'x', keys = '<Leader>' },
-
-                -- Built-in completion
-                { mode = 'i', keys = '<C-x>' },
-
-                -- `g` key
-                { mode = 'n', keys = 'g' },
-                { mode = 'x', keys = 'g' },
-
-                -- Marks
-                { mode = 'n', keys = "'" },
-                { mode = 'n', keys = '`' },
-                { mode = 'x', keys = "'" },
-                { mode = 'x', keys = '`' },
-
-                -- Registers
-                { mode = 'n', keys = '"' },
-                { mode = 'x', keys = '"' },
-                { mode = 'i', keys = '<C-r>' },
-                { mode = 'c', keys = '<C-r>' },
-
-                -- Window commands
-                { mode = 'n', keys = '<C-w>' },
-
-                -- `z` key
-                { mode = 'n', keys = 'z' },
-                { mode = 'x', keys = 'z' },
-            },
-
-            clues = {
-                -- enhance this by adding descriptions for <leader> mapping groups
-                miniclue.gen_clues.builtin_completion(),
-                miniclue.gen_clues.g(),
-                miniclue.gen_clues.marks(),
-                miniclue.gen_clues.registers(),
-                miniclue.gen_clues.windows(),
-                miniclue.gen_clues.z(),
-            },
-        })
-
-
         local statusline = function()
             local mini = require('mini.statusline')
 
